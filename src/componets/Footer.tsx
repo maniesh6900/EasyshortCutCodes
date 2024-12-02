c1cda798be7e99c4273edffc52e5c9856011e795
maniesh6900 <kaj882626@gmail.com>
2024-12-02 12:48:10 +0530

    added social links

--- src/componets/Footer.tsx
+++ src/componets/Footer.tsx
@@ -1,20 +1,33 @@
 import { Facebook, Github, Instagram, Twitter } from 'lucide-react'
-import { useNavigate } from 'react-router-dom'
-
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
-const navigate = useNavigate()
+
   return (
     <>
         <div className='relative h-screen'>
             <div className='absolute bottom-0 w-screen h-10 bg-amber-500 p-2 '>
-                <div className='flex pl-4 h-full w-1/2'>
+                <div className='flex pl-4 h-full w-1/4'>
                     <p className='text-white'>© 2023 All rights reserved.</p>
-                    <div className='text-white w-1/12  ml-14 flex justify-around'>
-                      <Instagram  onClick={()=>{navigate("https://www.instagram.com/maniesh_69x/")}} />
-                      <Facebook onClick={()=>{navigate("https://www.facebook.com/manish.ahir8822")}} />
-                      <Github onClick={()=>{navigate("https://github.com/maniesh6900")}}/>
-                      <Twitter onClick={()=>{navigate("https://x.com/Manish99493540")}} />
+                    <div className='text-white w-1/5 ml-14 flex justify-around'>
+                    <Link 
+                    to={"https://www.instagram.com/maniesh_69x"}>
+                      <Instagram />
+                    </Link>
+                    <Link 
+                    to={"https://www.facebook.com/manish.ahir8822"}>
+                    <Facebook />
+                    </Link>
+                    <Link 
+                    to={"https://github.com/maniesh6900"}>
+                    <Github />
+                    </Link>
+                    <Link to={"https://x.com/Manish99493540"}>
+                      <Twitter />
+                    </Link>
+                   
+                      
+                      
                     </div>
                 </div>
             </div>
